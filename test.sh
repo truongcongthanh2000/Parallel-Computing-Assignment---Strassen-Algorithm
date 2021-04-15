5983dfe91829607796f725a9bada1d2048f6312d
Truong Cong Thanh <truongcongthanh2000@gmail.com>
2021-04-15 21:04:33 +0700

    strassen algorithm by divide-and-conquer

--- test.sh
+++ test.sh
@@ -3,4 +3,9 @@
 echo "Gen matrix"
 g++ -std=c++11 gen.cpp -o gen
 
-./gen 4 10
+./gen 4 4 5 10
+
+echo "Run strassen algorithm by divide-and-conquer"
+
+g++ -std=c++11 strassen_divide-and-conquer.cpp -o strassen_divide-and-conquer
+./strassen_divide-and-conquer
