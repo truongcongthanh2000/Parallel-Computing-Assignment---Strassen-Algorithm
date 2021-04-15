6f35e760152b51aa55070f9563f6609ccdb4d387
Truong Cong Thanh <truongcongthanh2000@gmail.com>
2021-04-15 22:25:27 +0700

    update name_file

--- test.sh
+++ test.sh
@@ -7,10 +7,12 @@ g++ -std=c++11 gen.cpp -o gen
 
 echo "Run strassen algorithm by divide-and-conquer"
 
-g++ -std=c++11 strassen_divide-and-conquer.cpp -o strassen_divide-and-conquer
-./strassen_divide-and-conquer
+g++ -std=c++11 strassen_dac.cpp -o strassen_dac
+./strassen_dac
 
-echo "Run strassen algorithm by divide-and-conquer with another padding"
+echo "Run strassen algorithm by divide-and-conquer with lazy padding"
 
-g++ -std=c++11 strassen_divide-and-conquer_temp.cpp -o strassen_divide-and-conquer_temp
-./strassen_divide-and-conquer_temp
+g++ -std=c++11 strassen_dac_lazy-padding.cpp -o strassen_dac_lazy-padding
+./strassen_dac_lazy-padding
+
+rm *.exe
